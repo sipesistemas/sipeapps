8a608aabe8600aec9f678cc96e5060c7a102f301
Hugo Mesquita <hugohomesquita@gmail.com>
2019-11-05 16:49:51 -0300

    Ajustes

--- setup.py
+++ setup.py
@@ -1,3 +1,29 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='sipeapps',
+    version='3.0.4',
+    packages=['financeiro'],
+    include_package_data=True,
+    license='MIT License',
+    description='Conjunto de apps Sipe Sistemas',
+    url='http://github.com/sipesistemas',
+    author='Sipe Sistemas',
+    author_email='contato@sipesistemas.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=[
+        'Django>=2.2.7',
+        'djangorestframework>=3.10.3',
+    ]
+)
